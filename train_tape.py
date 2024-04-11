4d2ff1f098f13fba15a5a7c5dae350e8e940a6dd
Volodymyr Kyrylov <vol@wilab.org.ua>
2024-04-11 13:40:46 +0300

    train_tape: admit user-defined tape collections, add IxSupervision as a protocol for a tape

--- train_tape.py
+++ train_tape.py
@@ -7,61 +7,16 @@ from itertools import islice
 import numpy as np
 import torch
 import torch.nn.functional as F
+from typing import Protocol
 
 
-class Tapes:
-    choices = ["enwik8", "languini"]
-
-    @classmethod
-    def enwik8(cls, batch_size=32, seed=-1):
-        self = cls()
-        self.vocab_size = 205
-        self.seq_len = 512
-        self.train = Tape(np.memmap('data/enwik8.train', dtype=np.uint8, mode='r'), batch_size=batch_size, seq_len=self.seq_len, seed=seed)
-        self.valid = Tape(np.memmap('data/enwik8.val', dtype=np.uint8, mode='r'), batch_size=128, seq_len=self.seq_len)
-        self.test = Tape(np.memmap('data/enwik8.test', dtype=np.uint8, mode='r'), batch_size=128, seq_len=self.seq_len)
-        return self
-
-    @classmethod
-    def languini(cls, batch_size=32, **kwargs):
-        """
-        This tape provides access to training from the Languini Books dataset.
-        data/books directory is assumed to be available.
-        """
-        try:
-            from languini.dataset_lib.languini_books import LanguiniDatasetIterator
-        except ImportError as e:
-            raise ImportError("Install Languini Kitchen from: https://github.com/languini-kitchen/languini-kitchen") from e
-        self = cls()
-        self.vocab_size = 16384
-        self.seq_len = 512
-        self.train = LanguiniTape(LanguiniDatasetIterator(
-            data_path='data/books/books_16384',
-            split='train',
-            repeat=False,
-            global_batch_size=batch_size,
-            batch_idxs=range(batch_size),
-            micro_batches=1,
-            sequence_length=self.seq_len,
-            device='cuda',
-            end_of_doc_token=2,
-        ))
-        self.valid = LanguiniTape(LanguiniDatasetIterator(
-            data_path='data/books/books_16384',
-            split='test',
-            repeat=True,
-            global_batch_size=32,
-            batch_idxs=range(32),
-            micro_batches=1,
-            sequence_length=self.seq_len,
-            device='cuda',
-            end_of_doc_token=2,
-        ), max_batches=512)
-        self.test = self.valid
-        return self
+class IxSupervision(Protocol):
+    "A protocol for a sequence of batches. Tensors have shape (N, T)."
+    def __getitem__(self, step: int) -> tuple[torch.Tensor, torch.Tensor]:
+        ...
 
 
-class Tape:
+class Tape(IxSupervision):
     def __init__(self, data, batch_size, seq_len, seed=-1, device='cuda'):
         self.device = device
         self.data = data
@@ -97,7 +52,7 @@ class Tape:
         return F.pad(x, (0, self.seq_len - x.shape[0]), value=padding)
 
 
-class LanguiniTape:
+class LanguiniTape(IxSupervision):
     "Languini provides a streaming interface for data, this object wraps it into a random access-like interface."
     def __init__(self, iterator, max_batches=-1):
         self.iterator = iterator
@@ -116,3 +71,66 @@ class LanguiniTape:
         x, y, _ = next(self.iterator)
         self.step = step
         return x.squeeze(0).contiguous(), y.squeeze(0).contiguous() # squeeze the micro batch dimension
+
+
+class Tapes:
+    choices = ["enwik8", "languini"]
+    vocab_size: int
+    seq_len: int
+    train: IxSupervision
+    valid: IxSupervision
+    test: IxSupervision
+
+    def __init__(self, vocab_size: int, seq_len: int, train: IxSupervision, valid: IxSupervision, test: IxSupervision | None = None):
+        self.vocab_size = vocab_size
+        self.seq_len = seq_len
+        self.train = train
+        self.valid = valid
+        self.test = test or valid
+
+    @classmethod
+    def enwik8(cls, batch_size=32, seed=-1):
+        return cls(
+            vocab_size=205,
+            seq_len=512,
+            train=Tape(np.memmap('data/enwik8.train', dtype=np.uint8, mode='r'), batch_size=batch_size, seq_len=512, seed=seed),
+            valid=Tape(np.memmap('data/enwik8.val', dtype=np.uint8, mode='r'), batch_size=128, seq_len=512),
+            test=Tape(np.memmap('data/enwik8.test', dtype=np.uint8, mode='r'), batch_size=128, seq_len=512),
+        )
+
+    @classmethod
+    def languini(cls, batch_size=32, **kwargs):
+        """
+        This tape provides access to training from the Languini Books dataset.
+        data/books directory is assumed to be available.
+        """
+        try:
+            from languini.dataset_lib.languini_books import LanguiniDatasetIterator
+        except ImportError as e:
+            raise ImportError("Install Languini Kitchen from: https://github.com/languini-kitchen/languini-kitchen") from e
+        return cls(
+            vocab_size=16384,
+            seq_len=512,
+            train=LanguiniTape(LanguiniDatasetIterator(
+                data_path='data/books/books_16384',
+                split='train',
+                repeat=False,
+                global_batch_size=batch_size,
+                batch_idxs=range(batch_size),
+                micro_batches=1,
+                sequence_length=512,
+                device='cuda',
+                end_of_doc_token=2,
+            )),
+            valid=LanguiniTape(LanguiniDatasetIterator(
+                data_path='data/books/books_16384',
+                split='test',
+                repeat=True,
+                global_batch_size=32,
+                batch_idxs=range(32),
+                micro_batches=1,
+                sequence_length=512,
+                device='cuda',
+                end_of_doc_token=2,
+            ), max_batches=512)
+        )
